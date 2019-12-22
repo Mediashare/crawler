1a4572759dec6e34820ec87570da053642014c9f
Mediashare <mediashare.supp@gmail.com>
2019-12-22 21:04:58 +0100

    Opti & Perfomance

--- index.php
+++ index.php
@@ -2,6 +2,6 @@
 require 'vendor/autoload.php';
 
 use Mediashare\Crawler\Crawler;
-$crawler = new Crawler("http://marquand.pro");
+$crawler = new Crawler("http://marquand.pro/6/projet");
 $crawler->run();
 dump($crawler);
--- src/Crawler.php
+++ src/Crawler.php
@@ -3,6 +3,7 @@ namespace Mediashare\Crawler;
 
 use Mediashare\Scraper\Scraper;
 use Mediashare\Crawler\Entity\Url;
+use Mediashare\Crawler\Entity\Crawl;
 
 /**
  * Crawler
@@ -13,25 +14,37 @@ Class Crawler
 {
     public $url;
     public $urls = [];
+    public $crawled = [];
     function __construct(string $url) {
         $this->url = $url;
     }
 
     public function run() {
-        // First Scraping original url
-        $scraper = new Scraper($this->url);
+        // Scrape first page for get more urls
+        $scraper = $this->scrape($this->url);
+        $this->crawled[$this->url] = $scraper;
+
+        while (count($this->urls)) {
+            foreach ($this->urls as $url) {
+                $scraper = $this->scrape($url);
+                $this->crawled[(string) $url] = $scraper;
+                unset($this->urls[(string) $url]);
+            }
+        }        
+		return $this;
+    }
+
+    public function scrape(string $url): Scraper {
+        $scraper = new Scraper($url);
         $scraper->run();
-        $this->urls[$this->url] = $scraper;
         
-        // Scrape another url founded in original url
-        $urls = (array) $scraper->webpage->links;
-        foreach ($urls as $url) {
-            if ($url->isInternal): // Crawl internal url
-                $scraper = new Scraper($url);
-                $scraper->run();
-                $this->urls[(string) $url] = $scraper;        
+        // Add new urls
+        foreach ($scraper->webpage->links as $link) {
+            if ($link->isInternal && !isset($this->crawled[(string) $link])):
+                $this->urls[(string) $link] = (string) $link;
             endif;
         }
-		return $this;
+
+        return $scraper;
     }
 }
